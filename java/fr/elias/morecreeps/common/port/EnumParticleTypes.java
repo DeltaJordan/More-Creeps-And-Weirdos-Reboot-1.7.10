281758f13928a23ca6caa1b78ed0811ed7b32747
Astromojang <thegreatplanetmojang@gmail.com>
2016-05-06 19:22:36 -0500

    Finished port of EnumParticleTypes Need someone to fix the ones I typed out
    Example:
    particle("explode", ...    needs to be
    particle(EnumParticleType.EXPLOSION_NORMAL, ...
    and
    particle(EXPLOSION_NORMAL.toLowercase(), ... [Which isn't right anyway]
    needs to be
    particle(EnumParticleType.EXPLOSION_NORMAL, ...

--- java/fr/elias/morecreeps/common/port/EnumParticleTypes.java
+++ java/fr/elias/morecreeps/common/port/EnumParticleTypes.java
@@ -3,5 +3,46 @@ package fr.elias.morecreeps.common.port;
 public class EnumParticleTypes {
 	
 	public static String EXPLOSION_NORMAL = "explode";
+	public static String EXPLOSION_LARGE = "largeexplode";
+	public static String EXPLOSION_HUGE = "hugeexplosion";
+	public static String FIREWORKS_SPARK = "fireworksSpark";
+	public static String WATER_BUBBLE = "bubble";
+	public static String WATER_SPLASH = "splash";
+	public static String WATER_WAKE = "wake";
+    public static String SUSPENDED = "suspended";
+    public static String SUSPENDED_DEPTH = "depthsuspend";
+    public static String CRIT = "crit";
+    public static String CRIT_MAGIC = "magicCrit";
+    public static String SMOKE_NORMAL = "smoke";
+    public static String SMOKE_LARGE = "largesmoke";
+    public static String SPELL = "spell";
+    public static String SPELL_INSTANT = "instantSpell";
+    public static String SPELL_MOB = "mobSpell";
+    public static String SPELL_MOB_AMBIENT = "mobSpellAmbient";
+    public static String SPELL_WITCH = "witchMagic";
+    public static String DRIP_WATER = "dripWater";
+    public static String DRIP_LAVA = "dripLava";
+    public static String VILLAGER_ANGRY = "angryVillager";
+    public static String VILLAGER_HAPPY = "happyVillager";
+    public static String TOWN_AURA = "townaura";
+    public static String NOTE = "note";
+    public static String PORTAL = "portal";
+    public static String ENCHANTMENT_TABLE = "enchantmenttable";
+    public static String FLAME = "flame";
+    public static String LAVA = "lava";
+    public static String FOOTSTEP = "footstep";
+    public static String CLOUD = "cloud";
+    public static String REDSTONE = "reddust";
+    public static String SNOWBALL = "snowballpoof";
+    public static String SNOW_SHOVEL = "snowshovel";
+    public static String SLIME = "slime";
+    public static String HEART = "heart";
+    public static String BARRIER = "barrier";
+    public static String ITEM_CRACK = "iconcrack_";
+    public static String BLOCK_CRACK = "blockcrack_";
+    public static String BLOCK_DUST = "blockdust_";
+    public static String WATER_DROP = "droplet";
+    public static String ITEM_TAKE = "take";
+    public static String MOB_APPEARANCE = "mobappearance";
 
 }
